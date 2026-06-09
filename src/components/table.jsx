a1405325322007f35d0312cd51411c8a260cb3e1
Justin Patrick <jpatrick6420@gmail.com>
2026-06-08 21:34:13 -0600

    changed button row

--- src/components/table.jsx
+++ src/components/table.jsx
@@ -201,6 +201,8 @@ ${differenceText}
           </td>
 
           <td className="w-full text-center">
+            {tableData.theirPriceHome != "" ? "$" : ""}
+
             <input
               type="number"
               value={tableData.theirPriceHome}
@@ -281,7 +283,7 @@ ${differenceText}
           <td></td>
         </tr>
         <tr>
-          <td>
+          <td className="col-span-6 flex justify-center">
             <div className="flex gap-2">
               <button
                 className="bg-blue-500 hover:bg-blue-400 text-stone-50 px-2 py-1 rounded-md"
